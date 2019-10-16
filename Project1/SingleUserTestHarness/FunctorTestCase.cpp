e307a0b45f9e3992ef270429f5dd57f73bff29a3
mitchtown98 <56326737+mitchtown98@users.noreply.github.com>
2019-10-16 07:31:01 -0400

    Added FunctorTestCase.
    I compiled and ran this on my machine with no errors.

---
+++ Project1/SingleUserTestHarness/FunctorTestCase.cpp
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//	File:			FunctorTestCase.cpp															//
+//	Purpose:		FunctorTestCase driver for Test Runner to exectue							//
+//	Version:		1.0																			//
+//	Language:		C++, Visual Studio 2019														//
+//	Platform:		Windows 10																	//
+//	Application:	Single-User Test Harness, CSE687 - Object Oriented Design					//
+//	Author:			Laamont Harrington															//
+//					Aaron Meche																	//
+//					Chris Johnson																//
+//					Jason Mitchell																//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+	Maintenance History
+	===================
+	DATE		VERSION		DESCRIPTION
+	----		-------		----------------------------------------------------------------------
+	10/16/2019	1.0			Initial Creation
+*/
+#include <iostream>
+#include "FunctorTestCase.h"
+
+namespace CSE687_Project1 {
+	bool FunctorTestCase::execute() {
+		std::cout << " FunctorTestCase executing..." << std::endl;
+		return true;
+	}
+	REGISTER_TEST_CASE(FunctorTestCase);
+
+	bool Functor1::execute() {
+		std::cout << " Functor1 is currently executing " << std::endl;
+		Line();
+		
+		std::cout << " Functor1 testing an odd and even " <<std::endl;
+		operator()(3);
+		Line();
+		
+		operator()(8);
+		Line();
+
+		/*
+		 * I'm still learning the correct way to to perform a try block.
+		 * 
+		 try {
+			operator()(20.00004);
+		}
+		catch (const std::exception &exc) {
+			
+			std::cout << exc.what() << std::endl;
+			
+		} */
+		std::cout << " Functor1 passed and is leaving " << std::endl;
+		Line();
+		return true;
+	}
+	REGISTER_TEST_CASE(Functor1);
+}
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Project1/SingleUserTestHarness/FunctorTestCase.h
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//	File:			FunctorTestCase.h															//
+//	Purpose:		Derived class from TestBaseCase classe for the Single User Test Harness		// 
+//																								//
+//	Version:		1.0																			//
+//	Language:		C++, Visual Studio 2019														//
+//	Platform:		Windows 10																	//
+//	Application:	Single-User Test Harness, CSE687 - Object Oriented Design					//
+//	Author:			Laamont Harrington															//
+//					Aaron Meche																	//
+//					Chris Johnson																//
+//					Jason Mitchell																//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+	Maintenance History
+	===================
+	DATE		VERSION		DESCRIPTION
+	----		-------		----------------------------------------------------------------------
+	10/15/2019	1.0			Initial Creation
+*/
+#pragma once
+#include "TestCaseBase.h"
+
+namespace CSE687_Project1 {
+	class FunctorTestCase : public TestCaseBase {
+	public:
+		bool execute();
+	};
+
+	class Functor1 : public TestCaseBase {
+		bool result;
+	public:
+		Functor1() = default;
+		bool execute();
+		void operator()(int x) {
+			if (x % 2 == 0) {
+				std::cout << x << " Is Even " << std::endl;
+				
+			}else if (x % 2 != 0) {
+				std::cout << x << " Is Odd " << std::endl;
+			}
+			int j = sizeof int16_t;
+			if (x < 0 || x <= j){
+				//throw (std::make_exception_ptr(x));
+				result = false; 
+			}
+			return;
+		}
+		/*
+		 * Just something here to hold for errors.
+		 */
+		Functor1(std::ostream& output);
+	};
+	void Line() {
+		std::cout << "=====================" << std::endl;
+	}
+}
+
+
